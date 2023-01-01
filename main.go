2da13816d431de2f34334017d877a7db83eabb09
delta <delta@deltalab.dev>
2023-01-01 22:39:36 +0900

    asdf

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/hashicorp/hcl/v2/hclsimple"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+	"github.com/zclconf/go-cty/cty"
 )
 
 type SomeStructure struct {
@@ -21,5 +23,14 @@ func main() {
 	if err := hclsimple.DecodeFile("example.hcl", nil, &stru); err != nil {
 		panic(err)
 	}
-	fmt.Printf("result : %#v", stru)
+	fmt.Printf("decode result : %#v", stru)
+	f := hclwrite.NewEmptyFile()
+	b := f.Body()
+	b.SetAttributeValue("some_value", cty.StringVal("value"))
+	b.AppendNewline()
+	ebb := b.AppendNewBlock("some_struct", []string{"some_label"})
+	eb := ebb.Body()
+	eb.SetAttributeValue("some_value", cty.StringVal("value!"))
+	eb.SetAttributeValue("some_array", cty.ListVal([]cty.Value{cty.StringVal("element1"), cty.StringVal("element2")}))
+	fmt.Printf("%s\n", f.Bytes())
 }
